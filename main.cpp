289083c9dd941d4c5da0f547be291a3315fcfcbf
tobimd <33004093+tobimd@users.noreply.github.com>
2021-08-02 01:20:29 -0400

    WIP

--- main.cpp
+++ main.cpp
@@ -15,8 +15,71 @@
 #include <cstdlib>     // -- atoi
 #include <regex>       // -- regex_replace
 #include <ctime>       // 
+#include <vector>
+#include <limits>
 
 enum Param { Dist, Oppn };
+
+struct Array2D {
+	int **array;
+
+	std::size_t rows;
+	std::size_t cols;
+
+	Array2D() = default;
+	Array2D(const int __rows, const int __cols) {
+		rows = __rows;
+		cols = __cols;
+
+		array = new int*[rows];
+
+		for (std::size_t i = 0; i < rows; i++) {
+			array[i] = new int[cols];
+		}
+	}
+
+	// Manual call for destructor
+	void deleteAll() {
+		for (std::size_t i = 0; i < rows; i++) {
+			delete[] array[i];
+		}
+
+		delete[] array;
+	}
+
+	std::string ToString() {
+		std::string string = "\n[";
+
+		for (std::size_t i = 0; i < rows; i++) {
+			if (i != 0) {
+				string += " ";
+			}
+
+			string += "[";
+
+			for (std::size_t j = 0; j < cols; j++) {
+				string += " " + std::to_string(array[i][j]);
+			}
+
+			string += " ]";
+
+			if (i + 1 != rows) {
+				string += "\n";
+			}
+		}
+		string += "]\n";
+
+		return string;
+	}
+
+	int* operator[](const int index) {
+		return array[index];
+	}
+
+	int* operator[](const size_t index) {
+		return array[index];
+	}
+};
 
 // Generates a configuration for the complete problem
 // instance.
@@ -26,89 +89,32 @@ struct Configuration {
 	int max_iter;
 
 	int teams;
-	int **dist;
-	int **oppn;
+	Array2D dist;
+	Array2D oppn;
 
-	std::pair<std::size_t, std::size_t> distShape;
-	std::pair<std::size_t, std::size_t> oppnShape;
-
-	Configuration(int q1, int q2, int max_iter) {
+	Configuration(const int q1, const int q2, const int max_iter) {
 		Configuration::q1 = q1;
 		Configuration::q2 = q2;
 		Configuration::max_iter = max_iter;
 	}
 
 	~Configuration() {
-		for (std::size_t i = 0; i < distShape.first; i++) {
-			delete[] dist[i];
-		}
-
-		for (std::size_t i = 0; i < oppnShape.first; i++) {
-			delete[] oppn[i];
-		}
-
-		delete[] dist;
-		delete[] oppn;
+		dist.deleteAll();
+		oppn.deleteAll();
 	}
 
-	void CreateArrays(int __teams) {
+	void CreateArrays(const int __teams) {
 		teams = __teams;
 
-		distShape = std::pair<std::size_t, std::size_t>(teams, teams);
-		oppnShape = std::pair<std::size_t, std::size_t>(2*teams-2, teams);
-
-		dist = new int*[distShape.first];
-		oppn = new int*[oppnShape.first];
-
-		for (std::size_t i = 0; i < distShape.first; i++) {
-			dist[i] = new int[distShape.second];
-		}
-
-		for (std::size_t i = 0; i < oppnShape.first; i++) {
-			oppn[i] = new int[oppnShape.second];
-		}
+		dist = Array2D(teams, teams);
+		oppn = Array2D(2*teams - 2, teams);
 	}
 
-	std::string ToString(Param param) {
-		if (param == Param::Dist) {
-			return ToString(dist, distShape);
-
-		} else {
-			return ToString(oppn, oppnShape);
-
-		}
-
-	}
-
-	std::string ToString(int **array, std::pair<size_t, size_t> shape) {
-		std::string string = "\n[";
-
-		for (std::size_t i = 0; i < shape.first; i++) {
-			if (i != 0) {
-				string += " ";
-			}
-
-			string += "[";
-
-			for (std::size_t j = 0; j < shape.second; j++) {
-				string += " " + std::to_string(array[i][j]);
-			}
-
-			string += " ]";
-
-			if (i + 1 != shape.first) {
-				string += "\n";
-			}
-		}
-		string += "]\n";
-
-		return string;
-	}
 };
 
 // Recieves user input and generates a configuration
 // for the current problem instance
-void SetProblemParameters(char* input_file, Configuration *config) {
+void SetConfiguration(const char* input_file, Configuration *config) {
 
 	// Read instance data so that it can be stored in the
 	// Configuration struct `config`
@@ -205,6 +211,7 @@ void SetProblemParameters(char* input_file, Configuration *config) {
 		
 	}
 
+
 }
 
 void SimulatedAnnealingBestImprovement(Configuration *config) {
@@ -237,21 +244,173 @@ void SimulatedAnnealingBestImprovement(Configuration *config) {
 
 		end while
 	end while */
+}
+
+struct Match {
+	int ump = -1;
+	int home;
+	int visit;
+
+	double distance = 0.0;
+
+	Match() = default;
+	Match(int home, int visit) : home(home), visit(visit) {}
+
+	std::string ToString() {
+		return "<Match: home=" + std::to_string(home) + ", visit=" + std::to_string(visit) + ", ump=" + std::to_string(ump) + ">";
+	}
+};
+
+struct Slot {
+
+	// List of size 2 * teams - 2, consisting of each match between two teams
+	std::vector<Match> matches;
+
+	Slot() = default;
+	Slot(Configuration *config, const int time_slot) {
+		// Number of matches
+		int n_size = config->teams / 2;
+
+		matches.reserve(n_size); // Reserve known ammount of memory
+
+		// Iterate through opponents list, and create each match accordingly
+		for (int j = 0; j < n_size; j++) {
+
+			// If a positive value is found, then set `home` team as the current
+			// `j` index, and `visit` team as the value being hold in that position
+			if (config->oppn[time_slot][j] > 0)	{
+				matches.emplace(matches.end(), Match(j, config->oppn[time_slot][j]));
+			}
+		}
+	}
+
+	bool operator==(const Slot &other) {
+		if (matches.size() != other.matches.size()) return false;
+
+		for (std::size_t i = 0; i < matches.size(); i++) {
+			
+			if (matches[i].home != other.matches[i].home
+				|| matches[i].visit != other.matches[i].visit
+				|| matches[i].ump != other.matches[i].ump)	{
+
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	bool operator!=(const Slot &other) {
+		if (matches.size() != other.matches.size()) return true;
+
+		for (std::size_t i = 0; i < matches.size(); i++) {
+			
+			if (matches[i].home != other.matches[i].home
+				|| matches[i].visit != other.matches[i].visit
+				|| matches[i].ump != other.matches[i].ump)	{
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	std::string ToString() {
+		std::string string = "[" + matches[0].ToString();
+
+		for (std::size_t i = 1; i < matches.size(); i++) {
+			string += ", " + matches[i].ToString();
+		}
+
+		return string + "]";
+	}
+
+	void SetUmpires(int umps[]) {
+		for (int i = 0; i < matches.size(); i++) {
+			matches[i].ump = umps[i];
+		}
+	}
+};
+
+struct Instance {
+	std::vector<Slot> slots;
+};
+
+void GreedyMatchingHeuristic(Configuration *config) {
+	struct SolutionTree {
+		bool locked = false;
+
+		int time_slot;
+
+		std::vector<SolutionTree> nodes;
+
+		Slot slot;
+
+		SolutionTree(int __level, Configuration *config) {
+			slot = Slot(config, __level);
+			time_slot = __level;
+		}
+	};
+
+	std::size_t n_teams = config->teams;
+	std::size_t n_umps = n_teams / 2;
+	std::size_t n_costs = n_umps * n_umps;
+
+	int first_umps[n_umps];
+	for (int i = 0; i < n_umps; i++) {
+		first_umps[i] = i;
+
+	}
+
+	// Set root of the search tree
+
+	SolutionTree root = SolutionTree(0, config);
+	root.slot.SetUmpires(first_umps);
+	root.nodes.emplace(root.nodes.end(), SolutionTree(1, config));
+
+	SolutionTree *current = &root;
+	SolutionTree *next = &root.nodes[0];
+
+	// Costs of all possible matches per umpire
+	double assignment_costs[n_costs];
+	double prev_costs[n_costs];
+	for (std::size_t i = 0; i < n_costs; i++) {
+		assignment_costs[i] = std::numeric_limits<double>::infinity();
+		prev_costs[i] = std::numeric_limits<double>::infinity();
+	}
+
+	const double penalty = 10000;
+	double distance;
+	double incentive;
+	int violations;
+
+	Match curr_match;
+
+	int time_slots = 2 * config->teams - 2;
+	while (current->time_slot < time_slots) {
+
+		// Calculate costs for each edge, for each umpire
+		for (std::size_t ump = 0; ump < n_umps; ump++) {
+			for (std::size_t match = 0; match < n_umps; match++) {
+				curr_match = next->slot.matches[match];
+
+				distance = double(config->dist[curr_match.home][curr_match.visit]);
 
 
-	double temp_0 = 2000.0;
-	double TEMP_LIST = 500.0;
-	int N_ITER = config->max_iter;
-	double ALPHA = 0.95;
-	double p = 0.2;
-
-	int curr_iter = 0;
-
-	while (curr_iter < N_ITER) {
+			}
+		}
 		
 	}
 }
 
+void LocalSearch() {
+
+}
+
+void SimulatedAnnealing() {
+	
+}
 
 int main(int argc, char* argv[]) {
 	(void) argc;
@@ -260,6 +419,15 @@ int main(int argc, char* argv[]) {
 	 * Steps:
 	 * 		1. Get user input
 	 * 		2. Generate necessary objects related to problem instance
+	 * 			- q1
+	 * 			- q2
+	 * 			- max_iter
+	 * 			- init_temp
+	 * 			- min_temp
+	 * 			- temp_decrease_ratio (*)
+	 * 			- best_impv_ratio (*)
+	 * 			- 
+	 * 
 	 * 		3. Solve
 	 * 		4. Generate output
 	 */
@@ -271,8 +439,8 @@ int main(int argc, char* argv[]) {
 		std::atoi(argv[4])  // K (a.k.a. max_iter)
 	);
 
-	SetProblemParameters(argv[1], &config);
-	SimulatedAnnealingBestImprovement(&config);
+	SetConfiguration(argv[1], &config);
+	GreedyMatchingHeuristic(&config);
 
 	return 0;
 }
